5b544b01b14bf6443aef12d93dd3c862c1f8ccb9
jobtalle <jobtalle@hotmail.com>
2015-01-29 02:41:28 +0100

    Created general vector structure

--- test/test.c
+++ test/test.c
@@ -1,5 +1,21 @@
+#include <stdio.h>
+
 #include <ccVector/ccVector.h>
 
+CCV_DEFINE_VEC(1)
+
 int main(int argc, char **argv) {
+	ccvVec1 vec0, vec1, vec2;
+	vec0.elements[0] = 0;
+
+	CCV_SET(vec1, 0, 0.5f);
+	CCV_SET(vec2, 0, 0.2f);
+
+	ccvVec1Add(&vec0, vec1, vec2);
+
+	printf("%f\n", CCV_GET(vec0, 0));
+
+	getchar();
+
 	return 0;
 }
