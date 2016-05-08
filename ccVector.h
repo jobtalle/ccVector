e39ca87042962f521de2dfdb17944f47d9476e9a
jobtalle <jobtalle@hotmail.com>
2016-05-08 17:27:19 +0200

    Changed license to public domain

--- ccVector.h
+++ ccVector.h
@@ -6,31 +6,14 @@
 //                    | (_| (__ \  /  __/ (__| || (_) | |                           //
 //                     \___\___| \/ \___|\___|\__\___/|_| v1.1                      //
 //                                                                                  //
-//           Copyright (C) 2015 - 2016 \ Job Talle (jobtalle@hotmail.com)           //
+//                2015 - 2016 \ Job Talle (jobtalle@hotmail.com)                    //
 //__________________________________________________________________________________//
 //                                                                                  //
-// Redistribution and use in source and binary forms, with or without modification, //
-// are permitted provided that the following conditions are met:                    //
+//               ccVector has been dedicated to the public domain.                  //
 //                                                                                  //
-//  1. Redistributions of source code must retain the above copyright notice, this  //
-//     list of conditions and the following disclaimer.                             //
-//  2. Redistributions in binary form must reproduce the above copyright notice,    //
-//     this list of conditions and the following disclaimer in the documentation    //
-//     and / or other materials provided with the distribution.                     //
-//  3. Neither the name of the copyright holder nor the names of its contributors   //
-//     may be used to endorse or promote products derived from this software        //
-//     without specific prior written permission.                                   //
-//                                                                                  //
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND  //
-// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED    //
-// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE           //
-// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR //
-// ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL           //
-// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR       //
-// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER       //
-// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR //
-// TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF    //
-// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                //
+// Anyone is free to copy, modify, publish, use, compile, sell, or distribute this  //
+// software, either in source code form or as a compiled binary, for any purpose,   //
+//               commercial or non - commercial, and by any means.                  //
 //__________________________________________________________________________________//
 
 #ifndef CC_VECTOR
